2698c3e3cb46928e90ead2bca60d9c307bc739bb
Brodie Peif <brodiep21@hotmail.com>
2022-04-21 20:27:30 -0400

    socket error close

--- main.go
+++ main.go
@@ -14,77 +14,87 @@ import (
 	"github.com/bwmarrin/discordgo"
 )
 
-var Token string
+// Variables used for command line parameters
+var (
+	Token string
+)
 
-const goapiURL = "https://kutego-api-xxxxx-ew.a.run.app"
+const goAPIURL = "https://kutego-api-xxxxx-ew.a.run.app"
 
 func init() {
-	flag.StringVar(&Token, "t", "", "Bot 966488243895947264")
+	flag.StringVar(&Token, "t", "", "Bot Token")
 	flag.Parse()
 }
 
 func main() {
+
+	// Create a new Discord session using the provided bot token.
 	dg, err := discordgo.New("Bot " + Token)
 	if err != nil {
-		fmt.Println("Error creating Discord session,", err)
+		fmt.Println("error creating Discord session,", err)
 		return
 	}
+
 	// Register the messageCreate func as a callback for MessageCreate events.
 	dg.AddHandler(messageCreate)
 
 	// In this example, we only care about receiving message events.
 	dg.Identify.Intents = discordgo.IntentsGuildMessages
 
-	//Open a websocket Connect to Discord and start listening
+	// Open a websocket connection to Discord and begin listening.
 	err = dg.Open()
 	if err != nil {
-		fmt.Println("Error opening Connection to Discord,", err)
+		fmt.Println("error opening connection,", err)
 		return
 	}
 
-	fmt.Println("Bot is now running")
+	// Wait here until CTRL-C or other term signal is received.
+	fmt.Println("Bot is now running. Press CTRL-C to exit.")
 	sc := make(chan os.Signal, 1)
-	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, syscall.SIGTERM)
 	<-sc
 
+	// Cleanly close down the Discord session.
 	dg.Close()
 }
 
 type Gopher struct {
-	Name string `json: "name"`
+	Name string `json:"name"`
 }
 
 // This function will be called (due to AddHandler above) every time a new
 // message is created on any channel that the authenticated bot has access to.
 func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 
+	// Ignore all messages created by the bot itself
+	// This isn't required in this specific example but it's a good practice.
 	if m.Author.ID == s.State.User.ID {
 		return
 	}
 
 	if m.Content == "!gopher" {
 
-		//call the goapiURL and retrieve the Dr. Who gopher
-		response, err := http.Get(goapiURL + "/gopher/" + "dr-who")
+		//Call the KuteGo API and retrieve our cute Dr Who Gopher
+		response, err := http.Get(goAPIURL + "/gopher/" + "dr-who")
 		if err != nil {
 			fmt.Println(err)
 		}
-
 		defer response.Body.Close()
 
 		if response.StatusCode == 200 {
 			_, err = s.ChannelFileSend(m.ChannelID, "dr-who.png", response.Body)
 			if err != nil {
 				fmt.Println(err)
-			} else {
-				fmt.Println("Error: Can't get Dr.Who Gopher!")
 			}
+		} else {
+			fmt.Println("Error: Can't get dr-who Gopher! :-(")
 		}
 	}
 
 	if m.Content == "!random" {
 
-		response, err := http.Get(goapiURL + "/gopher/random/")
+		//Call the KuteGo API and retrieve a random Gopher
+		response, err := http.Get(goAPIURL + "/gopher/random/")
 		if err != nil {
 			fmt.Println(err)
 		}
@@ -96,44 +106,46 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 				fmt.Println(err)
 			}
 		} else {
-			fmt.Println("Error: Can't get a random gopher!")
+			fmt.Println("Error: Can't get random Gopher! :-(")
 		}
 	}
 
 	if m.Content == "!gophers" {
 
-		response, err := http.Get(goapiURL + "/gophers/")
+		//Call the KuteGo API and display the list of available Gophers
+		response, err := http.Get(goAPIURL + "/gophers/")
 		if err != nil {
 			fmt.Println(err)
 		}
-
 		defer response.Body.Close()
 
 		if response.StatusCode == 200 {
-			//Transform response to []byte
+			// Transform our response to a []byte
 			body, err := ioutil.ReadAll(response.Body)
 			if err != nil {
 				fmt.Println(err)
 			}
 
+			// Put only needed informations of the JSON document in our array of Gopher
 			var data []Gopher
 			err = json.Unmarshal(body, &data)
 			if err != nil {
 				fmt.Println(err)
 			}
 
+			// Create a string with all of the Gopher's name and a blank line as separator
 			var gophers strings.Builder
 			for _, gopher := range data {
 				gophers.WriteString(gopher.Name + "\n")
 			}
 
-			//Send a text message with the list of Gophers
+			// Send a text message with the list of Gophers
 			_, err = s.ChannelMessageSend(m.ChannelID, gophers.String())
 			if err != nil {
 				fmt.Println(err)
 			}
 		} else {
-			fmt.Println("Error: Can't get the list of Gophers!")
+			fmt.Println("Error: Can't get list of Gophers! :-(")
 		}
 	}
 }

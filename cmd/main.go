254596da4463abade866c9ecd5cc9c01b3879942
Brodie Peif <brodiep21@hotmail.com>
2023-03-29 15:25:22 -0400

    refactoring

--- cmd/main.go
+++ cmd/main.go
@@ -1,17 +1,54 @@
 package main
 
-import "github.com/brodiep21/discordbot/responses"
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/brodiep21/discordbot/messages"
+	"github.com/bwmarrin/discordgo"
+)
+
+// Variables used for command line parameters
+var (
+	Token string
+)
 
 func main() {
-	responses.RequestToChatGpt("hello!")
+
+	key := os.Getenv("apikey")
+
+	// Create a new Discord session using the provided bot token.
+	dg, err := discordgo.New("Bot " + key)
+	if err != nil {
+		fmt.Println("error creating Discord session,", err)
+		return
+	}
+	//TODO:
+
+	//set this function to run when gopherbot signs in
+	// responses.SignOnFunc()
+	// Register the messageCreate func as a callback for MessageCreate events.
+	dg.AddHandler(messages.MessageCreate)
+
+	// In this example, we only care about receiving message events.
+	dg.Identify.Intents = discordgo.IntentsGuildMessages
+
+	// Open a websocket connection to Discord and begin listening.
+	err = dg.Open()
+	if err != nil {
+		fmt.Println("error opening connection,", err)
+		return
+	}
+
+	// Wait here until CTRL-C or other term signal is received.
+	fmt.Println("Bot is now running.")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
+
+	// Close the Discord session.
+	dg.Close()
+
 }
-
-// {"id":"chatcmpl-6sHR9kxGsHF7EtR72QKh2RgGpARhJ","object":"chat.completion","created":1678394759,"model":"gpt-3.5-turbo-0301","usage":{"prompt_tokens":9,"completion_tokens":10,"total_tokens":19},
-// "choices":[{"message":{"role":"assistant","content":"\n\nHello! How can I assist you?"},"finish_reason":"stop","index":0}]}
-
-// package responses
-
-// check api
-
-// todo list
-// https://openai.com/blog/introducing-chatgpt-and-whisper-apis
